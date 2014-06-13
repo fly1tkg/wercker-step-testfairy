77f551d253a0853132069ff9a2b678ec71f1aa5d
Shoichi Takagi <fly1tkg@gmail.com>
2014-06-13 12:05:59 +0900

    fix environment variables

--- run.sh
+++ run.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
 # validate
-if [ ! -n "$FLY1TKG_TESTFAIRY_APK_FILE" ]
+if [ ! -n "$WERCKER_TESTFAIRY_APK_FILE" ]
 then
     fail "missing apk_file, please check your wercker.yml"
 fi
 
-if [ ! -e "$FLY1TKG_TESTFAIRY_APK_FILE" ]
+if [ ! -e "$WERCKER_TESTFAIRY_APK_FILE" ]
 then
     fail "apk file is not found, please check the apk file path"
 fi
 
-if [! -n "$FLY1TKG_TESTFAIRY_API_KEY" ]
+if [! -n "$WERCKER_TESTFAIRY_API_KEY" ]
 then
     fail "missing api_key, please check your wercker.yml"
 fi
 
-curl https://app.testfairy.com/api/upload/ -F api_key="$FLY1TKG_TESTFAIRY_API_KEY" -F apk_file=@"$FLY1TKG_TESTFAIRY_APK_FILE"
+curl https://app.testfairy.com/api/upload/ -F api_key="$WERCKER_TESTFAIRY_API_KEY" -F apk_file=@"$WERCKER_TESTFAIRY_APK_FILE"
