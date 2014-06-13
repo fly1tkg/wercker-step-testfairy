de707e739923b89f41b7f7909af161d5c92fd5dc
Shoichi Takagi <fly1tkg@gmail.com>
2014-06-13 14:29:24 +0900

    add optional params

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 validate_params() {
+    # Required parameters
     if [ ! -n "$WERCKER_TESTFAIRY_APK_FILE" ]
     then
         fail "missing apk_file, please check your wercker.yml"
@@ -17,10 +18,62 @@ validate_params() {
     fi
 }
 
+set_default_params_if_needed() {
+    #Optional parameters
+    if [ ! -n "$WERCKER_TESTFAIRY_METRICS" ]
+    then
+        WERCKER_TESTFAIRY_METRICS="cpu,memory,network,logcat,phone-signal,gps,battery"
+    fi
+
+    if [ ! -n "$WERCKER_TESTFAIRY_MAX_DURATION" ]
+    then
+        WERCKER_TESTFAIRY_MAX_DURATION="10m"
+    fi
+
+    if [ ! -n "$WERCKER_TESTFAIRY_VIDEO" ]
+    then
+        WERCKER_TESTFAIRY_VIDEO="on"
+    fi
+
+    if [ ! -n "$WERCKER_TESTFAIRY_VIDEO_QUALITY" ]
+    then
+        WERCKER_TESTFAIRY_VIDEO_QUALITY="high"
+    fi
+
+    if [ ! -n "$WERCKER_TESTFAIRY_VIDEO_RATE" ]
+    then
+        WERCKER_TESTFAIRY_VIDEO_RATE="1.0"
+    fi
+
+    if [ ! -n "$WERCKER_TESTFAIRY_ICON_WAETERMARK" ]
+    then
+        WERCKER_TESTFAIRY_ICON_WAETERMARK="off"
+    fi
+
+    if [ ! -n "$WERCKER_TESTFAIRY_COMMENT" ]
+    then
+        WERCKER_TESTFAIRY_COMMENT="Deploy of commit: $WERCKER_GIT_COMMIT from branch: $WERCKER_GIT_BRANCH"
+    fi
+}
+
 deploy_to_testfairy() {
+    if [ -n "$WERCKER_TESTFAIRY_PROGUARD_FILE" ]
+    then
+        PROGUARD_FILE_OPTION="-F proguard_file=@$WERCKER_TESTFAIRY_PROGUARD_FILE"
+    fi
+
     curl -3 https://app.testfairy.com/api/upload/ \
         -F api_key="$WERCKER_TESTFAIRY_API_KEY" \
-        -F apk_file=@"$WERCKER_TESTFAIRY_APK_FILE"
+        -F apk_file=@"$WERCKER_TESTFAIRY_APK_FILE" \
+        -F testers_groups="$WERCKER_TESTFAIRY_TESTERS_GROUPS" \
+        -F metrics="$WERCKER_TESTFAIRY_METRICS" \
+        -F max-duration="$WERCKER_TESTFAIRY_MAX_DURATION" \
+        -F video="$WERCKER_TESTFAIRY_VIDEO" \
+        -F video-quality="$WERCKER_TESTFAIRY_VIDEO_QUALITY" \
+        -F video-rate="$WERCKER_TESTFAIRY_VIDEO_RATE" \
+        -F icon-watermark="$WERCKER_TESTFAIRY_ICON_WAETERMARK" \
+        -F comment="$WERCKER_TESTFAIRY_COMMENT" \
+        $PROGUARD_FILE_OPTION
 }
 
 check_response() {
@@ -31,6 +84,7 @@ check_response() {
 }
 
 validate_params
+set_default_params_if_needed
 RES=`deploy_to_testfairy`
 info "TestFairy response: $RES"
 check_response $RES
