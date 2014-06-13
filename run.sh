2fe7b5b1864a3003d3ec07883d3962947642be9b
Shoichi Takagi <fly1tkg@gmail.com>
2014-06-13 12:21:28 +0900

    add white space and comment

--- run.sh
+++ run.sh
@@ -11,9 +11,10 @@ then
     fail "apk file is not found, please check the apk file path"
 fi
 
-if [! -n "$WERCKER_TESTFAIRY_API_KEY" ]
+if [ ! -n "$WERCKER_TESTFAIRY_API_KEY" ]
 then
     fail "missing api_key, please check your wercker.yml"
 fi
 
+# deploy to TestFairy
 curl https://app.testfairy.com/api/upload/ -F api_key="$WERCKER_TESTFAIRY_API_KEY" -F apk_file=@"$WERCKER_TESTFAIRY_APK_FILE"
